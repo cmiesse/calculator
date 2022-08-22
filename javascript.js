60b9843cd9c493973931b8c8e4d861ad8a38d68f
cmiesse <clement.miesse@gmail.com>
2022-08-22 14:51:36 +0200

    add functions for keys back clear equal

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+let calculation = [];
+let response = 0;
+let output;
+setTimeout(()=>{output = document.getElementById("output");},10)
+
 function add(num1,num2){
     return num1+num2;
 }
@@ -28,4 +33,34 @@ function operate(operator,num1, num2){
         default:
         return "Wrong operator";
     }
+}
+
+function addKey(key){
+    calculation.push(key);
+    output.textContent = calculation.join(" ");
+}
+
+function back(){
+    calculation.pop();
+    output.textContent = calculation.join(" ");
+}
+
+function clearOutput(){
+    calculation = [];
+    response = 0;
+    output.textContent = '';
+}
+
+function equal(){
+    const indexOperator = calculation.findIndex((i)=>i == '+' || i == '-' || i == '*' || i == '/')
+    const firstNumber = parseFloat(calculation.slice(0,indexOperator).join(""))
+    const operator = calculation[indexOperator];
+    const secondNumber =  parseFloat(calculation.slice(indexOperator+1).join(""))
+    if(operator == '/' && secondNumber == 0){
+        output.textContent = "You are wizard, Harry!";
+    } else{
+        response = operate(operator, firstNumber, secondNumber);
+        output.textContent = response;
+    }
+   
 }
